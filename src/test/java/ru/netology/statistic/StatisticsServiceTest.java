945b20b24a8dcace004be5a7f6098e4c70307fae
Maria Sedova <mariassedova@mail.ru>
2021-04-10 17:48:22 +0300

    tests up

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,31 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+
+  @Test
+  void findMaxWithAnotherArgs() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 25, 8, 6, 11, 11, 12};
+    long expected = 25;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void findMaxWithAnotherArgsVer1() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 25, 36, 6, 11, 11, 12};
+    long expected = 36;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+
+
 }
